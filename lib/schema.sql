5f41afe8880c4ba10f6b538939b4fdb5247a37b5
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 01:15:43 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -4,4 +4,8 @@ CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
 
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 
-CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, serie_id)
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id);
+
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
+
+CREATE TABLE 

c4a6ee082abcabf600afc5c82a8e3ec7d07bc7d1
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 02:09:00 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,9 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT DISTINCT(species), COUNT(species)
+  FROM characters
+  "
 end
 
 def select_name_and_series_subgenres_of_authors

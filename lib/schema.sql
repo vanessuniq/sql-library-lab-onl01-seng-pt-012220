1a68b9d48bb34941497bf76575029d69f6447b3d
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 01:08:21 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,1 +1,2 @@
-CREATE TABLE series(id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, )
+CREATE TABLE series(id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenre_id INTEGER);
+CREATE TABLE

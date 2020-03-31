3050a1a246fb5aaac5c60365d5cbe523db7feef3
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 02:26:23 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,11 +21,15 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name 
+  FROM authors 
+  INNER JOIN subgenres
+  WHERE authors.id = series.author_id and subgenres.id = series.subgenre_id;"
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT series.title FROM series 
+  "
 end
 
 def select_character_names_and_number_of_books_they_are_in

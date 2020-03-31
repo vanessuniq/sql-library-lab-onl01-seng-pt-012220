58aac90add1c06652198820c6aebf5016c755ff7
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 03:00:43 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -35,5 +35,9 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT characters.name, COUNT(book_id)"
+  "SELECT characters.name, COUNT(book_id)
+  FROM characters
+  INNER JOIN character_books
+  WHERE characters.id = character_books.character_id
+  GROUP BY "
 end

05447c93c9665a7eb64d24f7183965684d838b79
G. Vanessa Fotso <vanessuniq@gmail.com>
2020-03-31 01:17:32 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -8,4 +8,4 @@ CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id)
 
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER);
 
-CREATE TABLE characters
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, )
